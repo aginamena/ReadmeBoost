eaf3955c746b084efb868054c064db7f154a94ee
aginamena <aginamena5@gmail.com>
2025-05-02 08:54:57 -0400

    removed payload

--- app/repositories/page.tsx
+++ app/repositories/page.tsx
@@ -10,17 +10,12 @@ export default async function Repositories({
 }) {
   const { installation_id } = await searchParams;
   const octokit = await app.getInstallationOctokit(Number(installation_id));
-  const { data } = await octokit.request("GET /installation/repositories", {
-    per_page: 85,
-  });
+  const { data } = await octokit.request("GET /installation/repositories");
 
   return (
     <Container className="p-12">
       <Typography variant="h4" gutterBottom>
         My repositories
-      </Typography>
-      <Typography variant="body2" gutterBottom className="m-12">
-        Showing top 50 repos
       </Typography>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={{ xs: 2, md: 3 }}>
